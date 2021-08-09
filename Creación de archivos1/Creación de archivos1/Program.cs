2998aa4236df9b57a63609aa9deef42a8ed4b7d7
Ericka Rocío Talé Aguilera <201800083@colsanjose.edu.gt>
2021-08-09 13:10:53 -0600

    crear archivos

---
+++ Creación de archivos1/Creación de archivos1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creación_de_archivos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string R = "s";
+            while (R == "s")
+            {
+                Console.WriteLine("Escribe el nombre del arhivo");
+                string archivo = Console.ReadLine();
+                StreamWriter ar;
+                ar = File.CreateText(archivo + ".txt");
+                ar.WriteLine("Archivo Creado");
+                ar.Close();
+
+                Console.WriteLine("Quieres crear otro archivo?");
+                Console.WriteLine("s/n");
+                R = Console.ReadLine();
+            }
+        }
+    }
+}
